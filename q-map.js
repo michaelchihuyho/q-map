ef14439747f9418626b980385aa7765bfa6256ac
Michael Ho <michaelchihuyho@gmail.com>
2014-05-15 10:51:08 -0700

    Initial commit

---
+++ q-map.js
@@ -0,0 +1,36 @@
+var Q = require('q')
+  , async = require('async')
+  , _ = require('underscore')
+
+module.exports.map = function(myArray, iterator, limit) {
+
+    var queue
+      , promiseArray
+
+    limit = limit === undefined ? 1 : limit
+
+    queue = async.queue(function(task, callback) {
+        iterator(task.elem)
+            .then(function(result) {
+                task.deferred.resolve(result)
+                callback()
+            })
+            .fail(function(error) {
+                task.deferred.reject(error)
+                callback()
+            })
+    }, limit)
+
+    promiseArray = _.map(myArray, function(elem) {
+        var deferred = Q.defer()
+
+        queue.push({
+            deferred: deferred
+          , elem: elem
+        })
+
+        return deferred.promise
+    })
+
+    return promiseArray
+}
